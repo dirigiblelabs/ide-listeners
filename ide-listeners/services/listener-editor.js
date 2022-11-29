14de3d6695c7c3f364e85551fe67ebccdbaf2d4b
StanZGenchev <stan.z.genchev@gmail.com>
2022-11-29 16:52:21 +0200

    Migrated editor to the new view system

--- ide-listeners/services/listener-editor.js
+++ ide-listeners/services/listener-editor.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010-2020 SAP and others.
+ * Copyright (c) 2010-2022 SAP and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v2.0
  * which accompanies this distribution, and is available at
@@ -8,14 +8,16 @@
  * Contributors:
  *   SAP - initial API and implementation
  */
-exports.getEditor = function () {
-	return {
-		"id": "listener",
-		"name": "Listener",
-		"factory": "frame",
-		"region": "center-top",
-		"label": "Listener",
-		"link": "../ide-listeners/editor/editor.html",
-		"contentTypes": ["application/json+listener"]
-	};
+const editorData = {
+	id: "listener",
+	label: "Listener",
+	factory: "frame",
+	region: "center",
+	link: "../ide-listeners/editor/editor.html",
+	contentTypes: ["application/json+listener"]
 };
+if (typeof exports !== 'undefined') {
+	exports.getEditor = function () {
+		return editorData;
+	}
+}
